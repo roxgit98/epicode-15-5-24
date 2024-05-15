09c75fab05fafe798a25e1bc6d59ba0389d311db
Davide Giliberti <gilo.davide@hotmail.it>
2024-05-15 18:43:06 +0200

    aggiornamento esercizio 15-5-24

--- 15-5-24/js/script copy.js
+++ 15-5-24/js/script copy.js
@@ -1,12 +1,17 @@
 const inserireTesto = document.getElementById("inserireTesto");
 const bottone = document.getElementById("add");
 
+//funzione del bottone input
 const eventoBottone = function (event) {
+  const testoInput = inserireTesto.value;
   const nuovaLista = document.createElement("li");
-  const nuovoInput = document.createElement("p");
+  const nuovoP = document.createElement("p");
+  nuovoP.innerText = testoInput;
   const nuovoBottone = document.createElement("button");
+  nuovoBottone.setAttribute("type", "button");
+  nuovoBottone.innerText = "Cancella";
   const ul = document.getElementById("listaNonOrdinata");
   ul.appendChild(nuovaLista);
-  nuovaLista.appendChild(nuovoInput);
-  nuovoInput.appendChild(nuovoBottone);
+  nuovaLista.appendChild(nuovoP);
+  nuovoP.appendChild(nuovoBottone);
 };
